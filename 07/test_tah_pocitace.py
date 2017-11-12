9b84305d52521bc5db22ae21b66922a1981fee88
4d4nsoni4 <32848293+4d4nsoni4@users.noreply.github.com>
2017-11-12 20:36:27 +0100

    Create test_tah_pocitace.py

---
+++ 07/test_tah_pocitace.py
@@ -0,0 +1,12 @@
+import ai
+
+def test_tah_pocitace():
+    nove_policko = 4
+    pole = 20*"-"
+    def tah(pole, cislo_policka, symbol):
+        pole = pole[:cislo_policka-1] + symbol + pole[cislo_policka:]
+        return pole
+    pole = tah(pole, nove_policko, 'o')
+    spravne_pole = 3*"-"+"o"+16*"-"
+
+    assert spravne_pole == pole
