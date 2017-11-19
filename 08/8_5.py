6a13278d014e13fa37035aa703724905074e4adf
4d4nsoni4 <32848293+4d4nsoni4@users.noreply.github.com>
2017-11-19 15:11:13 +0100

    Create 8_5.py

---
+++ 08/8_5.py
@@ -0,0 +1,8 @@
+domaci_zvirata = ["pes", "kočka", "králík", "had"]
+
+def abeceda(seznam):
+    """ Seřadí seznam podle abecedy """
+    novy_seznam = sorted(domaci_zvirata)
+    print(novy_seznam)
+
+abeceda(domaci_zvirata)
